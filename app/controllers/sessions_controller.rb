3e306b36128af53b82d874cd3add132b7817d7bd
Aaron Rama <aaronrama@gmail.com>
2013-06-26 10:51:22 -0700

    Take out a stray fail

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,6 @@ class SessionsController < ApplicationController
     @user = User.find_by_username(params[:user][:username])
 
     if @user && @user.verify_password(params[:user][:password])
-      fail
       login_user(@user)
       redirect_to @user
     else
